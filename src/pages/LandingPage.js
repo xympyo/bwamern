788dfa4001e14dd5085d8b3c81e017bca6f3fa2d
Moshe Dayan <xynpyro@gmail.com>
2023-02-12 17:58:13 +0700

    Section Most Picked BWA Done

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+
 import Header from "../parts/Header";
-import landingPage from "../json/landingPage.json"
-import Hero from "../parts/Hero.js"
+import landingPage from "../json/landingPage.json";
+import Hero from "../parts/Hero.js";
+import MostPicked from "../parts/MostPicked.js";
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPage.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
       </>
     );
   }
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -8,22 +8,25 @@ import IconTrasure from "../assets/images/icons/camera.svg";
 
 import Button from "elements/Button";
 
+import formatNumber from '../utils/formatNumber.js'
+
 export default function Hero(props) {
   function showMostPicked() {
     window.scrollTo({
       top: props.refMostPicked.current.offsetTop - 30,
-      behavior: "smooth",
+      behavior: "smooth"
     });
   }
+
   return (
     <section className="container pt-4">
       <div className="row align-items-center">
         <div className="col-auto pr-5" style={{ width: 530 }}>
-          <h1 className="h2 font-weight-bold line-height-1 mb-3">
+          <h1 className="font-weight-bold line-height-1 mb-3">
             Forget Busy Work <br />
             Get Your Vacation
           </h1>
-          <p className="mb-5 font-weight-light text-gray-500 w-75">
+          <p className="mb-4 font-weight-light text-gray-500 w-75" style={{lineHeight:"170%"}}>
             We provide what you need to enjoy your holiday with family. Time to
             make another memorable moments. Your money can return, unlike your
             time.
@@ -36,8 +39,8 @@ export default function Hero(props) {
           >
             Show Me Now
           </Button>
-          <div className="row mt-5">
-            <div className="col-auto" style={{marginRight: 35}}>
+          <div className="row" style={{marginTop: 80}}>
+            <div className="col-auto" style={{ marginRight: 35 }}>
               <img
                 width="36"
                 height="36"
@@ -45,13 +48,14 @@ export default function Hero(props) {
                 alt={`${props.data.travelers} Travelers`}
               />
               <h6 className="mt-3">
-                {props.data.travelers}{" "}
-                <span className="text-gray-500 font-weight-light">Travelers</span>
+                {formatNumber(props.data.travelers)}{" "}
+                <span className="text-gray-500 font-weight-light">
+                  travelers
+                </span>
               </h6>
             </div>
-          </div>
-          <div className="row mt-5" style={{marginRight: 35}}>
-            <div className="col-auto">
+
+            <div className="col-auto" style={{ marginRight: 35 }}>
               <img
                 width="36"
                 height="36"
@@ -59,12 +63,10 @@ export default function Hero(props) {
                 alt={`${props.data.cities} Cities`}
               />
               <h6 className="mt-3">
-                {props.data.cities}{" "}
-                <span className="text-gray-500 font-weight-light">Cities</span>
+                {formatNumber(props.data.cities)}{" "}
+                <span className="text-gray-500 font-weight-light">cities</span>
               </h6>
             </div>
-          </div>
-          <div className="row mt-5">
             <div className="col-auto">
               <img
                 width="36"
@@ -73,10 +75,28 @@ export default function Hero(props) {
                 alt={`${props.data.treasures} Treasures`}
               />
               <h6 className="mt-3">
-                {props.data.treasures}{" "}
-                <span className="text-gray-500 font-weight-light">Treasures</span>
+                {formatNumber(props.data.treasures)}{" "}
+                <span className="text-gray-500 font-weight-light">
+                  treasures
+                </span>
               </h6>
             </div>
+          </div>
+        </div>
+        <div className="col-6 pl-5">
+          <div style={{width: 540, height: 370}}>
+            <img
+              src={ImageHero}
+              alt="Room with couches"
+              className="img-fluid position-absolute"
+              style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
+            ></img>
+            <img
+              src={ImageHero_}
+              alt="Room with couches frame"
+              className="img-fluid position-absolute"
+              style={{ margin: "0 -15px -15px 0", }}
+            ></img>
           </div>
         </div>
       </div>
