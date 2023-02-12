623dc9f4938d86feaa720c6edc1a5479e9c0d9a0
Moshe Dayan <xynpyro@gmail.com>
2023-02-12 18:37:32 +0700

    Finished component Most Picked and Categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import Header from "../parts/Header";
 import landingPage from "../json/landingPage.json";
 import Hero from "../parts/Hero.js";
 import MostPicked from "../parts/MostPicked.js";
+import Categories from "../parts/Categories.js";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -19,6 +20,7 @@ export default class LandingPage extends Component {
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
         />
+        <Categories data={landingPage.categories}></Categories>
       </>
     );
   }
