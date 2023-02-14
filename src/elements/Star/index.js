5a2190d74c5ba4cc6a60fe26822be661c7f1fa8a
Moshe Dayan <xynpyro@gmail.com>
2023-02-14 15:51:01 +0700

    Complete Component Parts + Footer, also fixed mistyped class

--- src/elements/Star/index.js
+++ src/elements/Star/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import propTypes from "prop-types";
 import "./index.scss";
 
-export default function Star(className, value, height, width, spacing) {
+export default function Star({className, value, height, width, spacing}) {
   const decimals = Number(value) % 1;
   const star = [];
   let leftPos = 0;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,6 +6,7 @@ import Hero from "../parts/Hero.js";
 import MostPicked from "../parts/MostPicked.js";
 import Categories from "../parts/Categories.js";
 import Testimony from '../parts/Testimony.js'
+import Footer from '../parts/Footer.js'
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -22,7 +23,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
-        <Testimony></Testimony>
+        <Testimony data={landingPage.testimonial}></Testimony>
+        <Footer/>
       </>
     );
   }
